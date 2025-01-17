315d5cb5345304442780bc334f9ff5e1f3a66a55
Hesham Elnabtity <hesham.elnabtity@student.tugraz.at>
2025-01-17 20:24:10 +0100

    added Makefile to src directory and adapted the settings to debug the program

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int sum()
+{
+    int x = 0;
+    x++;
+    return x+122;
+}
+
+int main()
+{   
+    int x = 1 , y = 0;
+    printf("Helooooo world %d\n", x);
+    printf("Helooooo world xxxx\n");
+    scanf("%d", &y);
+    printf("Helooooo world xxxx\n");
+
+    return 0;
+}
